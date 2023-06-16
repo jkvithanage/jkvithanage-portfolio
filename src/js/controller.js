6ee73d7441445fe2c6fbcbc5fa1bd17c471c1803
Janaka Vithanage <jkvithana@gmail.com>
2023-06-16 11:21:08 +1000

    build skills section

---
+++ src/js/controller.js
@@ -0,0 +1,5 @@
+// import * as icons from "simple-icons";
+
+// const langs = document.querySelector(".languages");
+// const markup = `<li class="skill-icon">${icons.siRuby.svg}</li>`;
+// langs.insertAdjacentHTML("afterbegin", markup);
